dc967ba8fb8cbd1d1b952b1bd6e4eb4c57d4af9c
HARISH <iamiam9278@gmail.com>
2026-09-18 13:11:30 +0530

    Update mount_uuid.sh

--- mount_uuid.sh
+++ mount_uuid.sh
@@ -2,27 +2,24 @@
 
 # ==========================================
 # Mount Filesystem Using UUID
-# Student Name:
-# Roll Number:
+# Student Name:harish.m.k
+# Roll Number:1u24it040
 # ==========================================
 
-
 # Display filesystem UUID
-
-
+blkid
 
 
 # Create mount directory
-
-
+mkdir -p /mnt/mydisk
 
 
 # Mount filesystem using UUID
 # Replace YOUR_UUID with actual UUID
-
-
+mount UUID=YOUR_UUID /mnt/mydisk
 
 
 # Display mounted filesystem
+df -h /mnt/mydisk
 
-
+exit 0
